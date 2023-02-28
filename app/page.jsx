1030fe10d2671eaa4e819e0400a51271a967a33b
sideeq12 <abdwaheed2018@gmail.com>
2023-02-28 09:19:02 +0000

    api data fetching : TMDB

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,12 @@
 
+"https://api.themoviedb.org/3/movie/popular"
 
-export default function Home() {
-  return (
+
+export default async function Home() {
+  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`);
+  const res = await data.json()
+  console.log(res)
+    return (
     <main>
       <h2 className='text-lg py-6'>Hello world</h2>
       <div>We are texting theb layout </div>

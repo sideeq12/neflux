42dbde82d28a9d97c487553c54a5e9e9c7947218
sideeq12 <abdwaheed2018@gmail.com>
2023-02-28 10:19:10 +0000

    dynmaic pages fix

---
+++ app/[movie]/page.jsx
@@ -0,0 +1,5 @@
+export default function MovieData(){
+    return (
+        <>Moive data</>
+    )
+}
--- app/movie.jsx
+++ app/movie.jsx
@@ -1,13 +1,13 @@
 import Link from "next/link"
 import Image from "next/image"
 
-export default function Movie({title, release_date, poster_path}){
+export default function Movie({title, id, release_date, poster_path}){
     const movie_path = "https://image.tmdb.org/t/p/original"
     return(
         <div>
             <h1>{title}</h1>
             <h2> {release_date}</h2>
-            <Link href={`/asd`}>
+            <Link href={`/${id}`}>
                 <Image src={movie_path+poster_path }
                 width={400}
                 height={400}

d6e4db40d7ebd2ba5d774ff444f15235915d6877
sideeq12 <abdwaheed2018@gmail.com>
2023-03-04 11:35:35 +0000

    build

--- app/page.jsx
+++ app/page.jsx
@@ -4,9 +4,11 @@ import Movie from "./movie";
 export default async function Home() {
   const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`);
   const res = await data.json()
+  console.log( res)
     return (
     <main>
-      <h2 className='text-lg py-6 font-mono font-extrabold lg:text-2xl'>Fleevid</h2>
+      <h2 className='text-lg py-6 font-mono font-extrabold lg:text-2xl'>Flee <span className="text-sky-400 
+      	text-decoration-line: overline decoration-pink-900">Video</span></h2>
       <div className="grid gap-16 grid-cols-fluid"> {res.results.map(
         data => <Movie 
         key={data.id}

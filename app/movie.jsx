739cefa276436676d5b7358b24398c8ae3ddc519
sideeq12 <abdwaheed2018@gmail.com>
2023-03-10 21:32:29 +0000

    threejs

--- app/movie.jsx
+++ app/movie.jsx
@@ -7,10 +7,10 @@ export default function Movie({title, id, release_date, poster_path}){
     const movie_path = "https://image.tmdb.org/t/p/original"
     return(
 
-        <div  className="mx-8 sm:mx-0" >
+        <div  className="mx-8 sm:mx-0">
             <h1>{title}</h1>
             <h2> {release_date}</h2>
-            
+
             <Link href={`/${id}`}>
                 <Image src={movie_path+poster_path }
                 width={400}

dbd96699d04b8354d767045335ff962541f33732
sideeq12 <abdwaheed2018@gmail.com>
2023-03-10 21:28:58 +0000

    threejs

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Movie from "./movie";
 
 // main function
+
 export default async function Home() {
   const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`);
   const res = await data.json()
